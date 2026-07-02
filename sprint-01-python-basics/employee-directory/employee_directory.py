c100ee742eef654481589a49af4c62b47e901a23
kumarasubramanian <kumarav.subramanian@gmail.com>
2026-07-02 14:38:45 -0700

    initial commit

---
+++ sprint-01-python-basics/employee-directory/employee_directory.py
@@ -0,0 +1,11 @@
+#Menu for employee directory
+print("Employee Directory")
+
+print("1. Add Employee")
+print("2. Search Employee")
+print("3. Update Salary")
+print("4. Delete Employee")
+print("5. Display All Employees")
+print("6. Exit")
+
+choice = int(input("Enter your choice:"))
