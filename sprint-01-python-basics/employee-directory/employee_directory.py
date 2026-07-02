0d5e87a250fa68878d9372d864f6cb142e082b85
kumarasubramanian <kumarav.subramanian@gmail.com>
2026-07-02 14:44:49 -0700

    added few conditions

--- sprint-01-python-basics/employee-directory/employee_directory.py
+++ sprint-01-python-basics/employee-directory/employee_directory.py
@@ -9,3 +9,36 @@ print("5. Display All Employees")
 print("6. Exit")
 
 choice = int(input("Enter your choice:"))
+
+# add employee
+employee_name = input("Enter employee name:")
+employee_salary = int(input("Enter Salary:))
+print("Employee added successfully.")
+
+#search Employee
+employee_name = input("Enter employee name:")
+
+print(f"Employee : {employee_name}")
+print(f"Salary   : ${salary}")
+
+# update salary
+
+Enter employee name:
+John
+
+Enter new salary:
+90000
+
+#Delete employee
+Enter employee name:
+John
+
+#display all employees
+
+Employee Directory
+
+John      $90,000
+
+Sarah     $82,000
+
+David     $74,500
